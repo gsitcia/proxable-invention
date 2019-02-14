03a29e6c04b95d3b43f8e09ffba126a0df02c9c3
Walter Mitty <gsitcia@gmail.com>
2019-02-14 00:41:25 -0600

    also wrong version

--- cdndexworker.js
+++ cdndexworker.js
@@ -1,36 +1,27 @@
-//self.importScripts("https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval-iife.min.js");
+self.importScripts("https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval-iife.min.js");
 self.addEventListener('fetch',function(event) {
-    event.respondWith((function (req) {
-        //return clients.matchAll().then(cl=>cl.length>0?new Response(cl[0].url):new Response(req.url));
-        //return new Response(req.url);
-        /*var url = new URL(req.url);
-        if (url.hostname == "127.0.0.1") {
-            return fetch(req.url);
+    //this new system only divides things into relative and absolute requests
+    //that means you can only surf around one origin
+    //base/redtoprox.html is special, it never gets proxied
+    //only two files are necessary, this and redtoprox.html, both at root
+    var url = new URL(event.request.url);
+    console.log(url.href);//
+    // I don't know why I decided to like codegolf this
+    event.respondWith(idbKeyval.get('real').then(real=>idbKeyval.get('fake').then(fake=>{
+        if (url.protocol == 'chrome-extension:') {
+            return fetch(url.href);//fix firebug and stuff
         }
-        /*return idbKeyval.get("url").then(url=>{
-            return new Response(`<p>${url}</p>`,{
-                headers: {"Content-Type":"text/html"}
-            });
-        });*/
-        if (req.url.indexOf('proxiy') > -1) {
-            //just a normal src="bob/joe" relative path
-            //return new Response(req.url);
-            return fetch('https://cors-anywhere.herokuapp.com/'+req.url.split('proxiy/')[1]);
-        } else {
-            //return new Response(req.url);
-            // they did a src="/blah" request possibly
-            var turl = new URL(req.url);
-            var path = turl.pathname;
-            return clients.matchAll().then(cl=>cl[0]).then(c=>{
-                //return new Response(c.url);/*
-                if ((new URL(c.url)).origin!=turl.origin) {//not a relative path
-                    return fetch('https://cors-anywhere.herokuapp.com/'+req.url);
-                }
-                var url = c.url.split('proxiy/')[1];
-                url = (new URL(url)).origin;
-                url = 'https://cors-anywhere.herokuapp.com/'+url;
-                return fetch(url+path);/**/
-            });
+        // we've got real and fake
+        if (url.origin == fake) {
+            // Either it was a relative url, or /redtoprox.html
+            if (url.pathname.indexOf('/redtoprox.html') > -1) {
+                return fetch(fake+'/redtoprox.html');
+            }
+            if (url.pathname.indexOf('/cdndexworker.js') > -1) {
+                return fetch(fake+'/cdndexworker.js');
+            }
+            return fetch(real+url.pathname+url.search);
         }
-    })(event.request));
+        return fetch('https://cors-anywhere.herokuapp.com/'+url.href);
+    })));
 });
